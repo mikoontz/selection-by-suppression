486c173a5085425b4e7b8f82499e94a3ecb76041
Michael Koontz <mikoontz@gmail.com>
2019-04-18 22:31:11 -0600

    not really selection effect on fire size; instead add the number of concurrent fires.

--- figures/figures_carpentry/selection-on-burning-conditions.R
+++ figures/figures_carpentry/selection-on-burning-conditions.R
@@ -11,13 +11,13 @@ ia <-
   dplyr::filter(prop_ypmc > 0.5)
 
 
-selection_on_event_size <-
-  ggplot(ia %>% dplyr::mutate(survived_ia = ifelse(survived_ia == 1, yes = "yes", no = "no")), aes(x = log(fire_area_m2 / 10000), fill = survived_ia)) +
+selection_on_concurrent_fires <-
+  ggplot(ia %>% dplyr::mutate(survived_ia = ifelse(survived_ia == 1, yes = "yes", no = "no")), aes(x = simultaneous_fires, fill = survived_ia)) +
   geom_density(alpha = 0.5) +
   # geom_histogram(alpha = 0.5, bins = 100) +
-  labs(x = expression(Fire ~ area ~ (log[10] ~ hectares)),
+  labs(x = "Number of fires concurrently burning\n(higher is more extreme)",
        fill = "Escaped/`survived`\ninitial attack") +
-  theme_bw()
+  theme_bw() + scale_fill_grey()
 
 selection_on_prefire_ndvi <-
   ggplot(ia %>% dplyr::mutate(survived_ia = ifelse(survived_ia == 1, yes = "yes", no = "no")), aes(x = prefire_ndvi, fill = survived_ia)) +
@@ -25,7 +25,7 @@ selection_on_prefire_ndvi <-
   # geom_histogram(alpha = 0.5, bins = 100) +
   labs(x = "Prefire NDVI\n(higher is more extreme)",
        fill = "Escaped/`survived`\ninitial attack") +
-  theme_bw()
+  theme_bw() + scale_fill_grey()
 
 selection_on_fuel_heterogeneity <-
   ggplot(ia %>% dplyr::mutate(survived_ia = ifelse(survived_ia == 1, yes = "yes", no = "no")), aes(x = nbhd_sd_ndvi_1, fill = survived_ia)) +
@@ -33,7 +33,7 @@ selection_on_fuel_heterogeneity <-
   # geom_histogram(alpha = 0.5, bins = 100) +
   labs(x = "Prefire NDVI heterogeneity\n(lower is more extreme)",
        fill = "Escaped/`survived`\ninitial attack") +
-  theme_bw()
+  theme_bw() + scale_fill_grey()
 
 selection_on_erc <-
   ggplot(ia %>% dplyr::mutate(survived_ia = ifelse(survived_ia == 1, yes = "yes", no = "no")), aes(x = prefire_erc, fill = survived_ia)) +
@@ -41,25 +41,25 @@ selection_on_erc <-
   # geom_histogram(alpha = 0.5, bins = 100) +
   labs(x = "Prefire Energy Release Component (ERC)\n(higher is more extreme)",
        fill = "Escaped/`survived`\ninitial attack") +
-  theme_bw()
+  theme_bw() + scale_fill_grey()
 
 selection_on_erc
 
-shared_legend = get_legend(selection_on_event_size)
+shared_legend = get_legend(selection_on_erc)
 
-selection_on_event_size + geom_density(data = fires_sdc %>% filter(objective == "wfu"), aes(x = log(fire_area_m2 / 10000)), inherit.aes = FALSE)
-selection_on_prefire_ndvi + geom_density(data = fires_sdc %>% filter(objective == "wfu"), aes(x = prefire_ndvi), inherit.aes = FALSE)
-selection_on_fuel_heterogeneity + geom_density(data = fires_sdc %>% filter(objective == "wfu"), aes(x = nbhd_sd_ndvi_1), inherit.aes = FALSE)
-selection_on_erc + geom_density(data = fires_sdc %>% filter(objective == "wfu"), aes(x = prefire_erc), inherit.aes = FALSE)
+selection_on_concurrent_fires + geom_density(data = fires %>% filter(objective == "wfu"), aes(x = simultaneous_fires), inherit.aes = FALSE)
+selection_on_prefire_ndvi + geom_density(data = fires %>% filter(objective == "wfu"), aes(x = prefire_ndvi), inherit.aes = FALSE)
+selection_on_fuel_heterogeneity + geom_density(data = fires %>% filter(objective == "wfu"), aes(x = nbhd_sd_ndvi_1), inherit.aes = FALSE)
+selection_on_erc + geom_density(data = fires %>% filter(objective == "wfu"), aes(x = prefire_erc), inherit.aes = FALSE)
 
-panel_grid <- plot_grid(selection_on_event_size + theme(legend.position = "none"),
-                        selection_on_prefire_ndvi + theme(legend.position = "none"),
+panel_grid <- plot_grid(selection_on_erc + theme(legend.position = "none"),
                         selection_on_fuel_heterogeneity + theme(legend.position = "none"),
-                        selection_on_erc + theme(legend.position = "none"),
+                        selection_on_concurrent_fires + theme(legend.position = "none"),
+                        selection_on_prefire_ndvi + theme(legend.position = "none"),
                         nrow = 2, ncol = 2,
                         labels = LETTERS[1:4])
 
 selection_panel_plot <- plot_grid(panel_grid, shared_legend, rel_widths = c(1, 0.1))
 selection_panel_plot
 
-ggsave(plot = selection_panel_plot, filename = "figures/selection-on-burning-conditions.pdf")
+ggsave(plot = selection_panel_plot, filename = "figures/selection-on-burning-conditions.png")
