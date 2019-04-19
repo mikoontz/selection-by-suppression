f5ee445768acde35c5909f58fb0014cb22b5898a
Michael Koontz <mikoontz@gmail.com>
2019-04-18 20:20:05 -0600

    start building a case for number of concurrent fires being an important driver of fire survivorship.

--- data/data_carpentry/busy-fires.R
+++ data/data_carpentry/busy-fires.R
@@ -9,121 +9,147 @@
 
 library(tidyverse)
 library(sf)
+library(lubridate)
 
-# Read metadata file
-md <- read_csv("data/data_raw/wildfire-severity_sierra-nevada-ca-usa_ypmc_1984-2017_fire-metadata.csv")
-# md_sf <- st_read("data/data_raw/wildfire-severity_sierra-nevada-ca-usa_ypmc_1984-2017_fire-metadata.geoJSON") %>% st_transform(3310)
-sdc <- 
-  read_csv("data/data_output/high-severity-fires-with-sdc.csv") %>% 
-  dplyr::select(fire_id, sdc)
-
-fires_by_fire <- read_csv("data/data_output/polygonized-fires-by-fire.csv")
-fires_by_severity <- read.csv("data/data_output/polygonized-fires-by-severity-classes.csv")
-
-# short_fires <- st_read("data/data_output/sierra-nevada-ypmc-short-fod.gpkg") %>% dplyr::rename(fod_id = FOD_ID)
-# ee_short_fires <- 
-#   st_read("data/data_output/ee_short-burning-conditions_48-day-window_L4578_bicubic.geojson") %>% 
-#   st_drop_geometry() %>% 
-#   dplyr::select(fod_id, alarm_date, cont_date, fire_size, prop_ypmc, elev, earlyFfm100, fm100, hdw, vs, vpd, RBR, preFire_ndvi, het_ndvi_1, focal_mean_ndvi_1)
-
-sev_cat_prop <-
-  fires_by_severity %>% 
-  dplyr::select(fire_id, sev_cat, sev_cat_prop) %>% 
-  tidyr::spread(key = sev_cat, value = sev_cat_prop, fill = 0) %>% 
-  dplyr::rename(prop_unchanged = `0`,
-                prop_low = `1`,
-                prop_mod = `2`,
-                prop_high = `3`)
+# Read data
+fires <- read_csv("data/data_output/merged-data-and-derived-variables.csv")
+wx_per_fire <- 
+  read_csv("data/data_output/wx_per_fire.csv") %>% 
+  dplyr::select(-`system:index`, -.geo, -alarm_date, -area_ha, -cont_date, -objective, -n_concur, -fire_name, -cause)
 
 fires <-
-  md %>% 
-  dplyr::mutate(burn_duration = as.numeric(ymd(paste(cont_year, cont_month, cont_day, sep = "-")) - ymd(paste(alarm_year, alarm_month, alarm_day, sep = "-")))) %>% 
-  left_join(fires_by_fire, by = "fire_id") %>% 
-  dplyr::left_join(sev_cat_prop, by = "fire_id") %>% 
-  dplyr::filter(!is.na(objective)) %>% 
-  dplyr::mutate(objective = ifelse(objective == 1, yes = "suppression", no = "wfu"))
-
-
-fires_sdc <-
   fires %>% 
-  left_join(sdc, by = "fire_id")
+  dplyr::left_join(wx_per_fire, by = "fire_id")
 
-# Successful initial attack versus failed initial attack based
-# on burn duration
 
-# Pier fire was definitely not contained in 2 days. Contained on 2017-11-29, not 2017-08-30?
-# 0000f67fc2d69fe5c384
-fires_sdc$cont_month[fires_sdc$fire_id == "0000f67fc2d69fe5c384"] = 11
-fires_sdc$cont_day[fires_sdc$fire_id == "0000f67fc2d69fe5c384"] = 29
-fires_sdc$burn_duration[fires_sdc$fire_id == "0000f67fc2d69fe5c384"] = 92
-
-# Cedar fire was definitely not contained the same day it started. Contained on 206-10-01
-# 000098483297cad8ed1f
-# http://cdfdata.fire.ca.gov/incidents/incidents_details_info?incident_id=1392
-fires_sdc$cont_month[fires_sdc$fire_id == "000098483297cad8ed1f"] = 10
-fires_sdc$cont_day[fires_sdc$fire_id == "000098483297cad8ed1f"] = 1
-fires_sdc$burn_duration[fires_sdc$fire_id == "000098483297cad8ed1f"] = 46
-
-# Round Fire in 2015 was 2015-02-06 to 2015-02-12; not just 2 days
-# 0000b2e869b63ddf6a4f
-# http://cdfdata.fire.ca.gov/incidents/incidents_details_info?incident_id=1073
-fires_sdc$alarm_day[fires_sdc$fire_id == "0000b2e869b63ddf6a4f"] = 6
-fires_sdc$cont_day[fires_sdc$fire_id == "0000b2e869b63ddf6a4f"] = 12
-fires_sdc$burn_duration[fires_sdc$fire_id == "0000b2e869b63ddf6a4f"] = 6 
-
-# Containment fire day estimated for Chilcoot Fire? Include?
-# 0000af58f72ef9d1239b
-
-# wfu <-
-#   fires_sdc %>% 
-#   dplyr::filter(objective == "wfu")
+# short <- st_read("data/data_output/ee_short-burning-conditions_48-day-window_L4578_bicubic.geojson")
 # 
-# quantile(wfu$burn_duration, prob = c(0.01, 0.05, 0.10, 0.25, 0.5), na.rm = TRUE)
+# short_mod <-
+#   short %>% 
+#   filter(!is.na(cont_date)) %>% 
+#   dplyr::rename(cont_date_milli_since_epoch = cont_date) %>% 
+#   dplyr::mutate(cont_year = year(milliseconds(cont_date_milli_since_epoch) + ymd("1970-01-01")),
+#                 cont_month = month(milliseconds(cont_date_milli_since_epoch) + ymd("1970-01-01")),
+#                 cont_day = day(milliseconds(cont_date_milli_since_epoch) + ymd("1970-01-01"))) %>% 
+#   dplyr::mutate(cont_date = ymd(paste(cont_year, cont_month, cont_day, sep = "-"))) %>%
+#   dplyr::mutate(alarm_date = ymd(paste(alarm_year, alarm_month, alarm_day, sep = "-"))) %>% 
+#   dplyr::mutate(burn_duration = as.numeric((cont_date - alarm_date))) %>% 
+#   dplyr::filter(burn_duration <= 364)  %>% 
+#   dplyr::mutate(dowy = ifelse(test = alarm_date < ymd(paste(alarm_year, "10", "01", sep = "-")), 
+#                               yes = 1 + alarm_date - ymd(paste(alarm_year - 1, "10", "01", sep = "-")),
+#                               no = 1 + alarm_date - ymd(paste(alarm_year, "10", "01", sep = "-")))) %>% 
+#   dplyr::mutate(doy = alarm_date - ymd(paste(alarm_year, "01-01", sep = "-"))) %>% 
+#   dplyr::mutate(survived_ia = ifelse(burn_duration > 1, yes = "yes", no = "no")) %>% 
+#   dplyr::mutate(survived_ia_code = ifelse(burn_duration > 1, yes = 1, no = 0))
+# 
+# short_mod$simultaneous_fires <- NA
+# 
+# for (i in seq_along(1:nrow(short_mod))) {
+#   alarm_date <- short_mod$alarm_date[i]
+#   short_mod$simultaneous_fires[i] <- length(which((short_mod$cont_date >= alarm_date) & (short_mod$alarm_date <= alarm_date)))
+# }
+# 
+# plot(table(short_mod$burn_duration))
 
-ia <-
-  fires_sdc %>% 
-  dplyr::filter(objective == "suppression") %>% 
-  dplyr::filter(!is.na(burn_duration) & (burn_duration >= 0 & burn_duration < 365)) %>% 
-  dplyr::mutate(survived_ia = ifelse(burn_duration > 1, yes = 1, no = 0))
+fires <- 
+  fires %>% 
+  dplyr::mutate(comparison_cat = ifelse(comparison_cat != "wfu" & simultaneous_fires > 20, yes = "june2008fire", no = comparison_cat)) %>% 
+  dplyr::mutate(comparison_cat = ifelse(comparison_cat == "june2008fire", yes = "busy_fire", no = comparison_cat))
 
-june2008fires_id <-
-  ia %>% 
-  dplyr::filter(alarm_year == 2008, alarm_month == 6, alarm_day %in% c(21, 22)) %>% 
-  pull(fire_id)
-
-fires_compare <-
-  fires_sdc %>% 
-  dplyr::filter(!is.na(burn_duration) & (burn_duration >= 0 & burn_duration < 365)) %>% 
-  dplyr::mutate(june2008fire = ifelse(fire_id %in% june2008fires_id, yes = "yes", no = "no")) %>% 
-  dplyr::left_join(ia %>% dplyr::select(fire_id, survived_ia)) %>% 
-  dplyr::mutate(comparison_cat = case_when(june2008fire == "yes" ~ "june2008fire",
-                                           objective == "wfu" ~ "wfu",
-                                           objective == "suppression" & survived_ia == 1 ~ "survived_ia",
-                                           objective == "suppression" & survived_ia == 0 ~ "died_ia"
-                                           ))
-
-
-
-
-fires_compare %>% group_by(comparison_cat) %>% summarize(n = n(),
-                                                         sdc = mean(sdc, na.rm = TRUE),
-                                                         nbhd_sd_ndvi_1 = mean(nbhd_sd_ndvi_1, na.rm = TRUE),
-                                                         prefire_erc = mean(prefire_erc, na.rm = TRUE),
-                                                         elev = mean(elevation, na.rm = TRUE),
-                                                         prop_high = mean(prop_high, na.rm = TRUE),
-                                                         fire_area_ha = mean(fire_area_m2, na.rm = TRUE) / 10000,
-                                                         burn_duration = mean(burn_duration, na.rm = TRUE),
-                                                         prefire_ndvi = mean(prefire_ndvi, na.rm = TRUE),
-                                                         prefire_fm100 = mean(prefire_fm100, na.rm = TRUE),
-                                                         prefire_vpd = mean(prefire_vpd, na.rm = TRUE),
-                                                         earlyfire_hdw = mean(earlyfire_hdw, na.rm = TRUE),
-                                                         earlyfire_vs = mean(earlyfire_vs, na.rm = TRUE)
-                                                         ) %>% 
+fires %>% 
+  dplyr::filter(prop_ypmc > 0.5) %>% 
+  group_by(comparison_cat) %>% summarize(n = n(),
+                                         elev = mean(elevation, na.rm = TRUE),
+                                         dowy = mean(dowy),
+                                         simultaneous_fires = mean(simultaneous_fires),
+                                         burn_duration = mean(burn_duration, na.rm = TRUE),
+                                         prefire_ndvi = mean(prefire_ndvi, na.rm = TRUE),
+                                         nbhd_sd_ndvi_1 = mean(nbhd_sd_ndvi_1, na.rm = TRUE),
+                                         prefire_erc = mean(prefire_erc, na.rm = TRUE),
+                                         erc_p10 = mean(erc_p10),
+                                         erc_p50 = mean(erc_p50),
+                                         erc_p90 = mean(erc_p90),
+                                         vpd_p10 = mean(vpd_p10),
+                                         vpd_p50 = mean(vpd_p50),
+                                         vpd_p90 = mean(vpd_p90),
+                                         vs_p10 = mean(vs_p10),
+                                         vs_p50 = mean(vs_p50),
+                                         vs_p90 = mean(vs_p90),
+                                         fm100_p10 = mean(fm100_p10),
+                                         fm100_p50 = mean(fm100_p50),
+                                         fm100_p90 = mean(fm100_p90),
+                                         hdw_p10 = mean(vs_p10 * vpd_p10),
+                                         hdw_p50 = mean(vs_p50 * vpd_p50),
+                                         hdw_p90 = mean(vs_p90 * vpd_p90),
+                                         prefire_fm100 = mean(prefire_fm100, na.rm = TRUE),
+                                         prefire_vpd = mean(prefire_vpd, na.rm = TRUE),
+                                         earlyfire_hdw = mean(earlyfire_hdw, na.rm = TRUE),
+                                         earlyfire_vs = mean(earlyfire_vs, na.rm = TRUE),
+                                         prop_high = mean(prop_high, na.rm = TRUE),
+                                         max_patch_area_high = mean(max_patch_m2_high, na.rm = TRUE) / 10000,
+                                         med_fire_area_ha = median(fire_area_m2, na.rm = TRUE) / 10000,
+                                         mean_fire_area_ha = mean(fire_area_m2, na.rm = TRUE) / 10000,
+                                         max_fire_area_ha = max(fire_area_m2, na.rm = TRUE) / 10000,
+                                         sdc = mean(sdc, na.rm = TRUE)
+                                         
+  ) %>% 
   as.data.frame()
 
+busy_fires <-
+  fires %>% 
+  dplyr::filter(comparison_cat == "busy_fire") %>% 
+  dplyr::mutate(group = cut(dowy, breaks = c(0, 150, 300, 365)))
 
+ggplot(busy_fires, aes(dowy, y = 1, color = group)) + geom_point()
+
+
+busy_fires %>% group_by(group) %>% summarize(n = n(),
+                                             survived_ia = mean(survived_ia),
+                                             elev = mean(elevation, na.rm = TRUE),
+                                             dowy = mean(dowy),
+                                             simultaneous_fires = mean(simultaneous_fires),
+                                             burn_duration = mean(burn_duration, na.rm = TRUE),
+                                             prefire_ndvi = mean(prefire_ndvi, na.rm = TRUE),
+                                             nbhd_sd_ndvi_1 = mean(nbhd_sd_ndvi_1, na.rm = TRUE),
+                                             erc_p10 = mean(erc_p10),
+                                             erc_p50 = mean(erc_p50),
+                                             erc_p90 = mean(erc_p90),
+                                             vpd_p10 = mean(vpd_p10),
+                                             vpd_p50 = mean(vpd_p50),
+                                             vpd_p90 = mean(vpd_p90),
+                                             vs_p10 = mean(vs_p10),
+                                             vs_p50 = mean(vs_p50),
+                                             vs_p90 = mean(vs_p90),
+                                             prefire_erc = mean(prefire_erc, na.rm = TRUE),
+                                             prefire_fm100 = mean(prefire_fm100, na.rm = TRUE),
+                                             prefire_vpd = mean(prefire_vpd, na.rm = TRUE),
+                                             earlyfire_hdw = mean(earlyfire_hdw, na.rm = TRUE),
+                                             earlyfire_vs = mean(earlyfire_vs, na.rm = TRUE),
+                                             prop_high = mean(prop_high, na.rm = TRUE),
+                                             max_patch_area_high = mean(max_patch_m2_high, na.rm = TRUE) / 10000,
+                                             med_fire_area_ha = median(fire_area_m2, na.rm = TRUE) / 10000,
+                                             mean_fire_area_ha = mean(fire_area_m2, na.rm = TRUE) / 10000,
+                                             max_fire_area_ha = max(fire_area_m2, na.rm = TRUE) / 10000,
+                                             sdc = mean(sdc, na.rm = TRUE)
+                                             
+) %>% 
+  as.data.frame()
+
+unique(busy_fires$alarm_month)
 june2008 <- fires_sdc %>% dplyr::filter(fire_id %in% june2008fires_id)
+
+sort(unique(busy_fires$alarm_date))
+
+busy_fires %>% 
+  dplyr::select(dowy, alarm_date) %>% 
+  dplyr::arrange(alarm_date) %>% 
+  as.data.frame()
 
 nrow(june2008)
 sum(june2008$fire_area_m2 / 10000 > 80)
 sum(june2008$fire_area_m2 / 10000 > 400)
+
+fires %>% 
+  dplyr::filter(prop_ypmc > 0.5) %>% 
+  ggplot(aes(dowy)) +
+  geom_histogram(bins = 365)
