ac2471f2e7541b961a29a479c759200639f03127
Michael Koontz <mikoontz@gmail.com>
2019-04-19 01:14:50 -0600

    png instead of pdf. update figures to correcy y axis of event size and to make one line dotted; density plot instead of histogram to show burn duration distribution

--- figures/figures_carpentry/burn-duration-plots-by-objective.R
+++ figures/figures_carpentry/burn-duration-plots-by-objective.R
@@ -4,82 +4,54 @@ library(sf)
 library(lubridate)
 library(cowplot)
 
-# Read metadata file
-md <- read_csv("data/data_raw/wildfire-severity_sierra-nevada-ca-usa_ypmc_1984-2017_fire-metadata.csv") %>% 
-  dplyr::mutate(alarm_date = ymd(paste(alarm_year, alarm_month, alarm_day, sep = "-")),
-                cont_date = ymd(paste(cont_year, cont_month, cont_day, sep = "-")),
-                burn_duration = as.numeric(cont_date - alarm_date))
-fires_by_severity <- read_csv("data/data_output/polygonized-fires-by-severity-classes.csv")
-
-sev_cat_prop <-
-  fires_by_severity %>% 
-  dplyr::select(fire_id, sev_cat, sev_cat_prop) %>% 
-  tidyr::spread(key = sev_cat, value = sev_cat_prop, fill = 0) %>% 
-  dplyr::rename(prop_unchanged = `0`,
-                prop_low = `1`,
-                prop_mod = `2`,
-                prop_high = `3`)
-
 fires <- 
-  read_csv("data/data_output/polygonized-fires-by-fire.csv") %>% 
-  dplyr::left_join(md, by = "fire_id") %>% 
-  dplyr::left_join(sev_cat_prop, by = "fire_id") %>% 
-  dplyr::filter(!is.na(objective)) %>% 
-  dplyr::mutate(objective = ifelse(objective == 1, yes = "suppression", no = "wfu"))
-
-sdc <-
-  read_csv("data/data_output/high-severity-fires-with-sdc.csv") %>%
-  dplyr::select(fire_id, sdc)
-
-fires_sdc <-
-  fires %>% 
-  dplyr::left_join(sdc, by = "fire_id") %>% 
-  dplyr::filter(burn_duration > 0,
-                burn_duration < 365)
+  read_csv("data/data_output/merged-data-and-derived-variables.csv")
 
 duration_distribution <-
-  ggplot(fires_sdc, aes(x = burn_duration, fill = objective)) +
-  geom_histogram(alpha = 0.5, bins = max(fires_sdc$burn_duration)) +
+  ggplot(fires, aes(x = burn_duration, fill = objective)) +
+  geom_density(alpha = 0.5) +
   theme_bw() +
   labs(x = "Burn duration (days)",
-       y = "Count",
+       y = "Density",
        fill = "Management\nobjective")
 
-ggsave(plot = duration_distribution, filename = "figures/burn-duration-density-plot-by-objective.pdf")
+ggsave(plot = duration_distribution, filename = "figures/burn-duration-density-plot-by-objective.png")
 
 event_size_v_duration <-
-  ggplot(fires_sdc, aes(x = burn_duration, y = fire_area_m2 / 10000, color = objective)) +
-  geom_point(alpha = 0.1) +
-  scale_y_log10(labels = function(x) format(x, scientific = TRUE)) +
+  ggplot(fires, aes(x = burn_duration, y = log(fire_area_m2 / 10000, base = 10), lty = objective, color = objective)) +
+  geom_point(alpha = 0.5) +
   geom_smooth() +
   theme_bw() +
   labs(x = "Burn duration (days)",
        y = expression(Event ~ size ~ (log[10] ~ hectares)),
+       lty = "Management\nobjective",
        color = "Management\nobjective")
-
-ggsave(plot = event_size_v_duration, filename = "figures/burn-duration-vs-event-size-by-objective.pdf")
+event_size_v_duration
+ggsave(plot = event_size_v_duration, filename = "figures/burn-duration-vs-event-size-by-objective.png")
 
 sdc_v_duration <-
-  ggplot(fires_sdc, aes(x = burn_duration, y = sdc, color = objective)) +
-  geom_point(alpha = 0.1) +
+  ggplot(fires, aes(x = burn_duration, y = sdc, lty = objective, color = objective)) +
+  geom_point(alpha = 0.5) +
   geom_smooth() +
   theme_bw() +
   labs(x = "Burn duration (days)",
        y = "Stand replacing\ndecay coefficient",
+       lty = "Management\nobjective",
        color = "Management\nobjective")
 
-ggsave(plot = sdc_v_duration, filename = "figures/burn-duration-vs-sdc-by-objective.pdf")
+ggsave(plot = sdc_v_duration, filename = "figures/burn-duration-vs-sdc-by-objective.png")
 
 prop_high_v_duration <- 
-  ggplot(fires_sdc, aes(x = burn_duration, y = prop_high, color = objective)) +
-  geom_point(alpha = 0.1) +
+  ggplot(fires, aes(x = burn_duration, y = prop_high, lty = objective, color = objective)) +
+  geom_point(alpha = 0.5) +
   geom_smooth() +
   theme_bw() +
   labs(x = "Burn duration (days)",
        y = "Proportion high severity",
+       lty = "Management\nobjective",
        color = "Management\nobjective")
 
-ggsave(plot = prop_high_v_duration, filename = "figures/burn-duration-vs-proportion-high-severity-by-objective.pdf")
+ggsave(plot = prop_high_v_duration, filename = "figures/burn-duration-vs-proportion-high-severity-by-objective.png")
 
 
 duration_distribution_no_legend <- duration_distribution + theme(legend.position = "none")
@@ -87,7 +59,7 @@ event_size_v_duration_no_legend <- event_size_v_duration + theme(legend.position
 sdc_v_duration_no_legend <- sdc_v_duration + theme(legend.position = "none") 
 prop_high_v_duration_no_legend <- prop_high_v_duration + theme(legend.position = "none")
 
-shared_legend <- get_legend(duration_distribution)
+shared_legend <- get_legend(sdc_v_duration)
 
 # build grid without legends
 pgrid <- plot_grid(duration_distribution_no_legend, 
@@ -99,4 +71,4 @@ pgrid <- plot_grid(duration_distribution_no_legend,
 # add legend
 panel_plot <- plot_grid(pgrid, shared_legend, ncol = 2, rel_widths = c(1, .1))
 
-ggsave(plot = panel_plot, filename = "figures/burn-duration-4-panel.pdf")
+ggsave(plot = panel_plot, filename = "figures/burn-duration-4-panel.png")
